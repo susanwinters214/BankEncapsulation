0c5fc20b6c76fa30487c6d59dc6606e341c3717f
Susann Winters <susanwinters214@gmail.com>
2023-11-09 15:13:47 -0600

    put up

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    { 
+
+                public BankAccount()
+                { 
+                }
+
+    private double balance = 0;
+
+   
+      
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating a deposit of {amount, 0:c} to your account!!");
+            balance = amount;
+        }
+        public double GetBalance() 
+        { 
+                return balance;
+        }
+   
+     }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -3,8 +3,23 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+  {
+            var myAccount = new BankAccount();
+
+            double userBalanace = myAccount.GetBalance();
+            Console.WriteLine($"Your userBalance before deposit is: {userBalanace}");
+
+            Console.WriteLine("How much $ do you want to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            myAccount.Deposit(amountToDeposit);  //ASSIGN INPUT TO A VARIABLE
+            Console.WriteLine($"Your amountToDeposit is: {amountToDeposit}");  //PRINT THE VALUE OF THE VARIABLE
+
+
+            double newBalance = userBalanace + amountToDeposit;
+
+            Console.WriteLine($"Your new balance is: {newBalance}");
+        
         }
     }
 }
